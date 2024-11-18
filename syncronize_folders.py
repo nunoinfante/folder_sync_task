a9b3252ce601b1553a145239a2752e2b8831f0c5
Nuno Infante <nunoinfante89@gmail.com>
2024-11-18 19:21:50 +0000

    synchronization and log file

--- syncronize_folders.py
+++ syncronize_folders.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from pathlib import Path
-import os, sys
+from filecmp import cmp
 import argparse
-import filecmp
+import shutil
+import sys
+import time
 
 def argumentParser():
     parser = argparse.ArgumentParser()
@@ -22,31 +25,43 @@ def argumentParser():
 
         log_path = Path(args['log'])
         if not log_path.is_file():
-            log_path.mkfile()
+            log_path.touch()
             print(f'Log file created: {log_path}')
         return source_path, replica_path, args['interval'], log_path
     else:
         print("Error: source directory doesn't exist")
         sys.exit(1)
 
-def synchronizeFolders(source, replica, log):
-
+def synchronizeFolders(source, replica, log_path):
     #Check and copy files from source to replica
     for file in source.glob("*"):
-        replica_file = replica / file.name
-        if replica_file.exists():
-            #comparar ficheiros usando filecmp / hash
-            #se foram iguais não há operacao . se forem diferentes copia ficheiro para replica
-            pass
+        replica_path = replica / file.name
+        if replica_path.exists():
+            if not cmp(source / file.name, replica / file.name):
+                shutil.copy(source / file.name, replica / file.name)        
+                log = f'Copied file {file.name} to replica'
+                writeLogFile(log_path, log)
         else:
-            #cria ficheiro na replica
-            pass
+            shutil.copy(source / file.name, replica / file.name)
+            log = f'Created file {file.name} in replica'
+            writeLogFile(log_path, log)
 
     #Check files from replica to source and delete those that dont exist
-
+    for file in replica.glob('*'):
+        source_path = source / file.name
+        if not source_path.exists():
+            replica_path = replica / file.name
+            replica_path.unlink()
+            log = f'Removed file {file.name} in replica'
+            writeLogFile(log_path, log)
 
 def writeLogFile(log_path, log):
-    pass
+    print(log)
+
+    f = open(log_path, 'a')
+    f.write(f'({datetime.now().strftime("%H:%M:%S")}) - {log}\n')
+    f.close
+    
 
 def main():
     source, replica, interval, log = argumentParser()
@@ -54,7 +69,7 @@ def main():
     try:
         print('Folder synchronization started')
         synchronizeFolders(source, replica, log)
-        print('Folder synchronization ended')
+        print('Folder synchronization ended\n')
 
     except KeyboardInterrupt:
         print('Folder synchronization interrupted')
